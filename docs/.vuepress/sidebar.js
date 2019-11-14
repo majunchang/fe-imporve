14414822f283c7cb2e13ab40d028edf17d3bec20
majunchang <majunchang@xiaomi.com>
2019-11-14 17:14:10 +0800

    增加js基础相关文章

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -2,6 +2,12 @@ module.exports = {
     '/algorithm/': [
         '/algorithm/',
         {
+            title: '常见',
+            children: [
+                '/algorithm/斐波那契数列'
+            ]
+        },
+        {
             title: '贪心算法',
             children: [
                 '/algorithm/greed'
@@ -17,13 +23,14 @@ module.exports = {
     // docs文件夹下面的accumulate文件夹 文档中md文件 书写的位置(命名随意)
     '/moveforward/': [
         {
-            title: 'JS基础',
+            title: 'JS',
             children: [
-                '/moveforward/js/1',
+                '/moveforward/js/防抖和节流',
                 '/moveforward/js/2',
                 '/moveforward/js/3',
                 '/moveforward/js/4',
                 '/moveforward/js/5',
+                '/moveforward/js/函数式编程',
             ]
         },
         {
--- docs/algorithm/index.md
+++ docs/algorithm/index.md
@@ -3,3 +3,5 @@
 [贪心算法](http://localhost:8080/fe-improve-deploy/algorithm/greed.html)
 
 [二分查找算法](http://localhost:8080/fe-improve-deploy/algorithm/binarySearch.html)
+
+[前端应该如何准备数据结构和算法](https://juejin.im/post/5d5b307b5188253da24d3cd1)
